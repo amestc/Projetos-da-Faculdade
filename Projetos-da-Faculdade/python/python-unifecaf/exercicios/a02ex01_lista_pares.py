43db7892ed2007cc7392160f211179ae43d205c2
Marcelo Ierardi <164015400+amestc@users.noreply.github.com>
2025-08-14 10:40:49 -0300

    Add files via upload

---
+++ Projetos-da-Faculdade/python/python-unifecaf/exercicios/a02ex01_lista_pares.py
@@ -0,0 +1,11 @@
+# Retorne apenas os números pares de uma lista de números
+# Exemplo:
+#   Entrada: [9, 4, 7 , 2, 1]
+#   Saída Experada: [4, 2]
+
+numeros = [9, 4, 7, 2, 1]
+
+for i in numeros:
+    if i % 2 == 0:
+        print(i, end=' ')
+print()  # Para garantir que a saída termine com uma nova linha
