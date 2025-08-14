bd06d183e48d906baf4a1abd6298d50f73d905c6
Marcelo Ierardi <164015400+amestc@users.noreply.github.com>
2025-08-14 10:40:13 -0300

    Delete python/python-unifecaf directory

--- python/python-unifecaf/exercicios/a02ex01_lista_pares.py
+++
@@ -1,4 +0,0 @@
-# Retorne apenas os números pares de uma lista de números
-# Exemplo:
-#   Entrada: [9, 4, 7 , 2, 1]
-#   Saída Experada: [4, 2]
--- python/python-unifecaf/exercicios/a02ex04_interseccao.py
+++
@@ -1,6 +0,0 @@
-# Retorne todos os números exitentes em ambas as listas
-# Exemplo:
-#   Entrada: 
-#       ListaA = [4, 9, 3 , 7, 8]
-#       Listab = [9, 5, 4 , 2, 1]
-#   Saída Experada: [9, 4]
